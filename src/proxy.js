2865b33cb6018b507fa85cfb1a7f5e65a115de12
Najmul-Huda70 <nh5696080@gmail.com>
2026-06-29 13:21:10 +0600

    comment proxy

--- src/proxy.js
+++ src/proxy.js
@@ -8,25 +8,25 @@ export async function proxy(request) {
 
   console.log('Detected Better Auth Token:', token);
 
-  try {
-    if (!token && pathname.startsWith("/dashboard")) {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  // try {
+  //   if (!token && pathname.startsWith("/dashboard")) {
+  //     return NextResponse.redirect(new URL('/login', request.url));
+  //   }
 
-    if (token && pathname === "/login") {
-      return NextResponse.redirect(new URL('/', request.url)); 
-    }
-    if (token && pathname === "/register") {
-      return NextResponse.redirect(new URL('/', request.url)); 
-    }
-  } catch (error) {
-    console.error("Middleware Auth Error:", error);
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+  //   if (token && pathname === "/login") {
+  //     return NextResponse.redirect(new URL('/', request.url)); 
+  //   }
+  //   if (token && pathname === "/register") {
+  //     return NextResponse.redirect(new URL('/', request.url)); 
+  //   }
+  // } catch (error) {
+  //   console.error("Middleware Auth Error:", error);
+  //   return NextResponse.redirect(new URL('/', request.url));
+  // }
 
   return NextResponse.next();
 }
  
 export const config = {
-  matcher: ['/dashboard/:path*', '/login','/register'] 
+  // matcher: ['/dashboard/:path*', '/login','/register'] 
 };
